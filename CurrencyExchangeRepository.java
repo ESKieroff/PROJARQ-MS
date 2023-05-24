9344795d0daa70acf37a7c0ec045fc42db540599
ESKieroff <Rodrigo.Rosa81@edu.pucrs.br>
2023-05-24 23:30:09 +0000

    initial commit

---
+++ CurrencyExchangeRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyExchangeRepository extends CrudRepository<CurrencyExchange,Long> {
+    CurrencyExchange findByFromAndTo(String from, String to);
+}
+
